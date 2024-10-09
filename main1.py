ddfb6d75528a02893c543ab85e800b01230f0237
MLOps-MaitriAI <hayyanshah111@gmail.com>
2024-10-09 15:50:19 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -117,7 +117,7 @@ async def login_page():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Login</title>
+        <title>LOG IN</title>
         <style>
             body {
                 font-family: Arial, sans-serif;
